28621f60d47f60ad1aba8c4932860c02160570ae
MIHIR MITHANI <157868082+techavenger123@users.noreply.github.com>
2024-09-27 11:42:06 +0530

    Create ADM1.cpp

--- ADM1.cpp
+++ ADM1.cpp
@@ -2,22 +2,22 @@
 #include <vector>
 using namespace std;
 
-vector<int> adaptiveDeltaModulation(vector<int>& signal, int initialStepSize, int stepAdjustmentFactor) {
+vector<int> adaptiveDeltaModulation(vector<int>& dt, int initial, int factor) {
     vector<int> modulated;
-    int previousValue = 0;
-    int stepSize = initialStepSize;
+    int pre = 0;
+    int st = initial;
 
-    for (int i = 0; i < signal.size(); i++) {
-        if (signal[i] > previousValue) {
-            modulated.push_back(1);previousValue += stepSize;
-            stepSize += stepAdjustmentFactor;
+    for (int i = 0; i < dt.size(); i++) {
+        if (dt[i] > pre) {
+            modulated.push_back(1);pre += st;
+            st += factor;
         } else {
             modulated.push_back(0);
-            previousValue -= stepSize;
-            stepSize += stepAdjustmentFactor;
+            pre -= st;
+            st += factor;
         }
         if (i > 0 && modulated[i] != modulated[i-1]) {
-            stepSize = max(initialStepSize, stepSize - stepAdjustmentFactor);
+            st = max(initial, st - factor);
         }
     }
 
@@ -25,13 +25,13 @@ vector<int> adaptiveDeltaModulation(vector<int>& signal, int initialStepSize, in
 }
 
 int main() {
-    vector<int> signal = {0, 1, 4, 8, 12, 18, 15, 10, 5};
-    int initialStepSize = 1;
-    int stepAdjustmentFactor = 1;
-    vector<int> modulatedSignal = adaptiveDeltaModulation(signal, initialStepSize, stepAdjustmentFactor);
-    cout << "Adaptive Modulated signal: ";
-    for (int i = 0; i < modulatedSignal.size(); i++) {
-        cout << modulatedSignal[i] << " ";
+    vector<int> dt = {0, 1, 4, 8, 12, 18, 15, 10, 5};
+    int initial = 1;
+    int factor = 1;
+    vector<int> dm = adaptiveDeltaModulation(dt, initial, factor);
+    cout << "Adaptive Modulated dt: ";
+    for (int i = 0; i < dm.size(); i++) {
+        cout << dm[i] << " ";
     }
     cout << endl;
 
